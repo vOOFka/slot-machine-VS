d1a3e14559ca8dbd96122064a6245acc760912e1
Vladimir Sirel <exit551@ya.ru>
2022-06-30 00:11:22 +0300

    Created project

---
+++ slot-machine-VS/slot-machine-VS/slot_machine_VSApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_machine_VSApp.swift
+//  slot-machine-VS
+//
+//  Created by Home on 30.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct slot_machine_VSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
