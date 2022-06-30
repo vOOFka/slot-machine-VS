0d37b520c1e4feef4bfa70f17976a866907e16a4
Vladimir Sirel <exit551@ya.ru>
2022-06-30 20:12:19 +0300

    Added emoji and button

--- slot-machine-VS/slot-machine-VS/ContentView.swift
+++ slot-machine-VS/slot-machine-VS/ContentView.swift
@@ -8,16 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emodjiArray = ["⭐️", "☀️", "💦", "🍉", "⛄️", "🐳", "🐥", "✂️", "🎁", "☎️"]
+    
     var body: some View {
         HStack {
-            Text("#1")
+            Text(getRandomEmoji())
                 .padding(20.0)
-            Text("#2")
+            Text(getRandomEmoji())
                 .padding(20.0)
-            Text("#3")
+            Text(getRandomEmoji())
                 .padding(20.0)
         }
+        Button(action: {
+            playButtonTap()
+        }, label: {
+            Text("Play")
+                .padding(5.0)
+                .frame(width: 100.0)
+        })
+           // .disabled(username.isEmpty || password.isEmpty)
+            .foregroundColor(.white)
+            .accentColor(.black)
+            .background(.mint)
+            .cornerRadius(5.0)
+            .padding(.top, 30.0)
     }
+    
+    private func getRandomEmoji() -> String {
+        emodjiArray.randomElement() ?? "🎲"
+     }
+    
+    private func playButtonTap() {
+        print("Play button tap")
+     }
 }
 
 //struct ContentView_Previews: PreviewProvider {
